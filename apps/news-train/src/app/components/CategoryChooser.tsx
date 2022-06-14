063f4e550da927c4cffb43205971e732221576c4
Cole Albon <cole@cafe-society.news>
2022-06-14 14:06:21 -0700

    collapsable categories on posts

---
+++ apps/news-train/src/app/components/CategoryChooser.tsx
@@ -0,0 +1,95 @@
+import React, {useContext, FunctionComponent, ComponentPropsWithoutRef } from 'react';
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Collapse
+} from '@mui/material';
+import {ExpandLess, ExpandMore} from '@mui/icons-material';
+
+import { CategoriesContext } from './CategoriesFetch';
+
+interface CategoryChooserProps extends ComponentPropsWithoutRef<"button"> {
+  handleCategoryClick: (index: string) => void
+  handlePageIndexClick: (index: string) => void
+  selectedCategoryIndex: string
+  selectedPageIndex: string
+  labelOrEcho: (index: string) => string
+}
+
+const CategoryChooserCategories: FunctionComponent<CategoryChooserProps> = (props: CategoryChooserProps) => {
+  const {
+    handleCategoryClick, 
+    handlePageIndexClick, 
+    selectedCategoryIndex
+  } = {...props}
+
+  const fetchedCategoriesContext = useContext(CategoriesContext);
+  const categories = structuredClone(fetchedCategoriesContext)
+
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+      <List>
+        <ListItem key={'category_chooser_allcategories'} disablePadding>
+          <ListItemButton 
+            // sx={{ pl: 4 }} 
+            key={'category_chooser_button_allcategories'}
+            onClick={() => {
+              handleClick()
+              handleCategoryClick('allCategories')
+              handlePageIndexClick('posts')
+              }
+            }
+          >
+            <ListItemText key={'CategoryChooserAllCategories'} primary={'Posts'} />
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+        </ListItem>
+
+        <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+        {
+          Object.entries(categories)
+          .filter(categoryItem =>  Object.assign(categoryItem[1] as object).checked === true)
+          .map((categoryItem) => {
+            return (
+              <ListItem key={`category_chooser_${categoryItem[0]}`} disablePadding>
+                <ListItemButton 
+                  sx={{ pl: 4 }} 
+                  key={`category_chooser_button_${categoryItem[0]}`}
+                  disabled={`${categoryItem[0]}` === `${selectedCategoryIndex}`} 
+                  onClick={() => {
+                      handleCategoryClick(`${categoryItem[0]}`)
+                      handlePageIndexClick('posts')
+                    }
+                  }
+                > 
+                  {
+                    [Object.entries(categoryItem[1] as object)
+                    .filter(attributeItem => attributeItem[0] === 'label')
+                    .map(attributeItem => attributeItem[1])
+                    .filter(label => {
+                      return !!label
+                    })
+                    .concat(categoryItem[0])
+                    .find(() => true)]
+                    .flat()
+                    .map((labelOrIndex: string) => <ListItemText key={labelOrIndex} primary={labelOrIndex} />)
+                  }
+                </ListItemButton>
+              </ListItem>
+            )
+          })
+        }
+        </List>
+      </Collapse>
+  </List>
+  );
+}
+export default CategoryChooserCategories
