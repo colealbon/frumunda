987ba5a0f4ad643c694779be7857a203b61e9f0a
Cole Albon <cole@cafe-society.news>
2022-06-23 17:51:30 -0700

    rename function netlify-backend -> axios-fetch

--- apps/axios-fetch/jest.config.ts
+++ apps/axios-fetch/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'backend-netlify',
+  displayName: 'axios-fetch',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/backend-netlify',
+  coverageDirectory: '../../coverage/apps/axios-fetch',
 };
