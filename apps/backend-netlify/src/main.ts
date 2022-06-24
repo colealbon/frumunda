dcad8ed56875c07e0d353c78a49902d25c6239d1
Cole Albon <cole@cafe-society.news>
2022-06-23 17:14:36 -0700

    .netlify/functions/main?url=http://www.google.com works

--- apps/backend-netlify/src/main.ts
+++ apps/backend-netlify/src/main.ts
@@ -1,11 +1,22 @@
 import { Handler } from "@netlify/functions";
+import axios from  'axios'
 
 const handler: Handler = async (event, context) => {
-    console.log('hello')
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
-  };
-};
+  return axios.get(event.queryStringParameters.url)
+  .then(response => {
+    return ({
+      statusCode: 200,
+      body: `${response.data}`
+    })
+  })
+  .catch(err => {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
+    }
+  })
+}
 
 export { handler };
