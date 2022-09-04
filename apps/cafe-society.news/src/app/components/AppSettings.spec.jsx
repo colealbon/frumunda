40f4423dd13ed9d4202a759792c4675e81b0662b
Cole Albon <cole@cafe-society.news>
2022-09-04 14:51:30 -0700

    unit test - AppSettings

--- apps/cafe-society.news/src/app/components/AppSettings.spec.jsx
+++ apps/cafe-society.news/src/app/components/AppSettings.spec.jsx
@@ -1,19 +1,43 @@
-import ReactDOM from 'react-dom/client';
+import {Suspense} from 'react';
+import '@testing-library/jest-dom'
 import AppSettings from './AppSettings'
+import { render, screen, waitFor } from '@testing-library/react';
+import "fake-indexeddb/auto";
 
-let container;
-
-beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
+describe('AppSettings', () => {
+  describe('AppSettings component', () => {
+    beforeEach( async () => {
+      render(<Suspense fallback={'loading'}><AppSettings /></Suspense>)
+    })
+    it ('renders settings hide processed posts', async () => {
+      await waitFor(() => {
+        const target = screen.getByText('hide processed posts');
+        expect(target).toBeInTheDocument()
+      })
+    })
+    it ('renders settings disable machine learning', async () => {
+      await waitFor(() => {
+        const target = screen.getByText('disable machine learning');
+        expect(target).toBeInTheDocument()
+      })
+    })
+    it ('renders settings ignore machine learning under', async () => {
+      await waitFor(() => {
+        const target = screen.getByText('ignore machine learning under 100 documents');
+        expect(target).toBeInTheDocument()
+      })
+    })
+    it ('renders settings machine learning confidence threshold', async () => {
+      await waitFor(() => {
+        const target = screen.getByText('machine learning confidence threshold: .99');
+        expect(target).toBeInTheDocument()
+      })
+    })
+    it ('renders reset button', async () => {
+      await waitFor(() => {
+        const target = screen.getByText('reset app settings');
+        expect(target).toBeInTheDocument()
+      })
+    })
+  });
 });
-
-afterEach(() => {
-  document.body.removeChild(container);
-  container = null;
-});
-
-it('can render and update a classifier component', () => {
-    ReactDOM.createRoot(container).render(<AppSettings />);
-});
-
