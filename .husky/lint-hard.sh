6ca84c7c7bcfef50b23d7ba2c75543c9a40334f5
Cole Albon <cole@cafe-society.news>
2022-09-26 13:48:36 -0700

    add unit tests

--- .husky/lint-hard.sh
+++ .husky/lint-hard.sh
@@ -1,4 +1,4 @@
-filelist=$(git diff --cached --name-only --diff-filter=d | grep -E '.(jsx|tsx|jsx|tsx|vue)$')
+filelist=$(git diff --cached --name-only --diff-filter=d | grep -v ".spec" | grep -E '.(jsx|tsx|jsx|tsx|vue)$')
 
 if [ `echo $filelist | wc -w` -lt 1 ]; then
     echo -e "You have no staged .js or .vue files to test\n"
