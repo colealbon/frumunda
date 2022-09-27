df9e6d8a753cda8ce5e2101457369b6d6650a642
Cole Albon <cole@cafe-society.news>
2022-09-27 10:57:20 -0700

    test for categoryChooser

--- apps/cafe-society.news/src/app/components/CategoryChooser.spec.jsx
+++ apps/cafe-society.news/src/app/components/CategoryChooser.spec.jsx
@@ -1,19 +1,47 @@
-import ReactDOM from 'react-dom/client';
+import { Suspense } from 'react';
+import '@testing-library/jest-dom'
 import CategoryChooser from './CategoryChooser'
+import { render, screen, act, waitFor } from '@testing-library/react';
+import user from '@testing-library/user-event'
 
-let container;
+describe('CategoryChooser', () => {
+  describe('CategoryChooser component', () => {
+    beforeEach(async () => {
+      await act(async () => await render(<Suspense fallback={'loading'}><CategoryChooser /></Suspense>))
+    })
+    it ('renders checked category', async () => {
+      await waitFor(() => {
+        const getText = screen.getByText(/business/i)
+        expect(getText).not.toBeNull()
+      })
+    })
+    it ('does not render unchecked category ', async () => {
+      await waitFor(() => {
+        expect(screen.findByText(/maritime/i).toBeUndefined)
+      })
+    })
 
-beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
+    it ('click posts shuts the drawer', async () => {
+      await waitFor(() => {
+        const getText = screen.getByText(/business/i)
+        expect(getText).not.toBeNull()
+      })
+      const selectAllCategories = screen.findByText('/posts/i')
+      user.click(selectAllCategories)
+      await waitFor(() => {
+        expect(screen.findByText(/business/i).toBeUndefined)
+      })
+    })
+    // it ('click icon opens the drawer', async () => {
+    //   const openDrawerButton = screen.getByRole('button', {
+    //     name: /open drawer/i
+    //   })
+    //   // user.click(openDrawerButton)
+    //   // await waitFor(() => {
+    //   //   expect(true).toEqual(true)
+    //   //   // const getText = screen.getByText(/posts/i)
+    //   //   // expect(getText).toBeVisible()
+    //   // })
+    // })
+  })
 });
-
-afterEach(() => {
-  document.body.removeChild(container);
-  container = null;
-});
-
-it('can render and update a classifier component', () => {
-    ReactDOM.createRoot(container).render(<CategoryChooser />);
-});
-
