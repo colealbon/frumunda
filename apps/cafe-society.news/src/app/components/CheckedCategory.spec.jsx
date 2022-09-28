c022f149ad86366269dc378d2268e9f1eb7bcb48
Cole Albon <cole@cafe-society.news>
2022-09-28 13:53:31 -0700

    test for CheckedCategory component

--- apps/cafe-society.news/src/app/components/CheckedCategory.spec.jsx
+++ apps/cafe-society.news/src/app/components/CheckedCategory.spec.jsx
@@ -1,19 +1,31 @@
-import ReactDOM from 'react-dom/client';
-import CheckedCategory from './CheckedCategory'
+import { Suspense, useContext} from 'react';
+import '@testing-library/jest-dom'
+import Category, {CategoryContext} from './CheckedCategory'
+import { render, screen, act, waitFor } from '@testing-library/react';
 
-let container;
-
-beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
+describe('CheckedCategory', () => {
+  describe('CheckedCategory component', () => {
+    const DisplayCategory = () => {
+      const categoryContext = useContext(CategoryContext);
+      const category = `${categoryContext}`;
+      return (
+        <div>{category}</div>
+    )}
+    beforeEach(async () => {
+      await act(async () => await render(<Suspense fallback={'loading'}><Category><DisplayCategory /></Category></Suspense>))
+    })
+    it ('skips unchecked category', async () => {
+      await waitFor(() => {
+        expect(screen.findByText(/maritime/i).toBeUndefined)
+      })
+    })
+    it ('renders checked category', async () => {
+      await waitFor(() => {
+        const findCategory = screen.getByText(/science/i)
+        expect(findCategory).not.toBeNull()
+      })
+    })
+  })
 });
 
-afterEach(() => {
-  document.body.removeChild(container);
-  container = null;
-});
-
-it('can render and update a classifier component', () => {
-    ReactDOM.createRoot(container).render(<CheckedCategory />);
-});
 
